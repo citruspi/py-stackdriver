f40e60fe0645232d806880b380db8548e55c9d2b
Mihir Singh <git.service@mihirsingh.com>
2015-05-10 19:43:10 -0400

    If a username or api_key isn't passed in, fall back to environment variables

--- stackdriver/client.py
+++ stackdriver/client.py
@@ -1,5 +1,15 @@
+import os
+
 class Client(object):
 
-    def __init__(self, username, api_key):
+    authorization = None
+
+    def __init__(self, username=None, api_key=None):
+        if username is None:
+            username = os.environ.get('STACKDRIVER_USERNAME', None)
+
+        if api_key is None:
+            api_key = os.environ.get('STACKDRIVER_API_KEY', None)
+
         self.authorization = (username, api_key)
 
