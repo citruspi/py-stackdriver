96c05611ca3421319ab1a3ac517d03581e25bf2b
Mihir Singh <git.service@mihirsingh.com>
2015-05-10 19:54:12 -0400

    Fell back to a configuration file if the credentials are in the environment

--- stackdriver/client.py
+++ stackdriver/client.py
@@ -1,4 +1,5 @@
 import os
+import ConfigParser
 
 class Client(object):
 
@@ -10,6 +11,16 @@ class Client(object):
 
         if api_key is None:
             api_key = os.environ.get('STACKDRIVER_API_KEY', None)
+
+        if username is None or api_key is None:
+            config = ConfigParser.RawConfigParser()
+            config.read(os.path.expanduser('~/.stackdriver'))
+
+            if username is None:
+                username = config.get('Credentials', 'username')
+
+            if api_key is None:
+                api_key = config.get('Credentials', 'api_key')
 
         self.authorization = (username, api_key)
 
